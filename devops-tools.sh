e29125d5421b95cc951f4e39ea168807f622ab0d
Marvin Korir <marvinkorir.mk@gmail.com>
2023-03-26 09:47:12 +0300

    added helm & helmfile devops tools

--- devops-tools.sh
+++ devops-tools.sh
@@ -28,10 +28,17 @@ echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
 
-#install Minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
-sudo rpm -Uvh minikube-latest.x86_64.rpm
+#install Minikube if it does not exist
+if ! command -v minikube &> /dev/null
+then
+    # Minikube is not installed, so download and install it
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+    sudo rpm -Uvh minikube-latest.x86_64.rpm
+fi
+
+# Start Minikube with the qemu driver
 minikube start --driver=qemu
+rm -rf minikube-latest.x86_64.rpm
 
 #install ansible
 sudo dnf install -y ansible
@@ -46,11 +53,7 @@ terraform -install-autocomplete
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-
-#install helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+rm -rf aws*
 
 #install Jetbrains toolbox
 # Download the latest version of JetBrains Toolbox
@@ -77,3 +80,22 @@ curl -LO https://github.com/ahmetb/kubectx/releases/latest/download/kubens
 chmod +x kubectx kubens
 # Move the files to a directory in your PATH
 sudo mv kubectx kubens /usr/local/bin/
+
+#Install eksctl
+Download and extract the latest release of eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+Move the extracted binary to /usr/local/bin
+sudo mv /tmp/eksctl /usr/local/bin
+
+#Install helm
+sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+#Install helmfile
+curl -LO https://github.com/roboll/helmfile/releases/download/v0.144.0/helmfile_linux_amd64
+# Make the binary executable
+chmod +x helmfile_linux_amd64
+# Move the binary to a directory in your PATH
+sudo mv helmfile_linux_amd64 /usr/local/bin/helmfile
+# Verify the installation
+helmfile version
+
